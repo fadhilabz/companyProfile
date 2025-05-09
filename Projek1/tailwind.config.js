743831df8b690bb6dba9bc884b69bc942dd9a3a0
fadhilabz <fadhilgithub@gmail.com>
2025-05-09 20:45:35 +0800

    first commit

---
+++ Projek1/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./navbar.html",           // Tambahkan ini jika kamu pakai file langsung
+    "./src/**/*.{html,js}",    // Tambahkan ini kalau file ada di folder src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
